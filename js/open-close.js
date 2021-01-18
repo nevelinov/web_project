4a0570bfbc72629f985930d7a3043460d8f8ee27
yavka <yavor_98@abv.bg>
2021-01-18 23:15:07 +0200

    Notify me to double click

--- js/open-close.js
+++ js/open-close.js
@@ -10,3 +10,15 @@ function closeForm(event, elementId) {
 	if (event.target.id === elementId)
     document.getElementById(elementId).style.display = "none";
 }
+
+function showMessage(event, elementId) {
+    if (event.target.id !== elementId) return;
+    let span = document.getElementById("notify");
+    span.style.display = "flex";
+    span.style.top = `${event.clientY - 50}px`;
+    span.style.left = `${event.clientX - 80}px`;
+
+    setTimeout(()=>{
+        span.style.display = "none";
+    }, 275);
+}
