614f7da9e3d8466d4c8234fc7f74d0351ec6a559
yavka <yavor_98@abv.bg>
2021-01-18 21:37:27 +0200

    Estimate And Priority

--- js/navigate-between-estimations.js
+++ js/navigate-between-estimations.js
@@ -1,14 +1,35 @@
-var arr = [1, 3, 4, 5, 7];
-var cur = 0;
+let arr;
+let index;
 
 function next() {
-	const estimation = document.getElementById('estimation-info-text');
-	cur = cur == arr.length - 1 ? 0 : cur + 1;
-	estimation.value = arr[cur];
+	const estimationText = document.getElementById('estimation-info-text');
+	const estimationValue = document.getElementById('est-value-disabled');
+	index = index == arr.length - 1 ? 0 : index + 1;
+	estimationValue.value = arr[index].value;
+	estimationText.value = arr[index].text
 }
 
 function prev() {
-	const estimation = document.getElementById('estimation-info-text');
-	cur = cur == 0 ? arr.length - 1 : cur - 1;
-	estimation.value = arr[cur];
+	const estimationText = document.getElementById('estimation-info-text');
+	const estimationValue = document.getElementById('est-value-disabled');
+	index = index == 0 ? arr.length - 1 : index - 1;
+	estimationValue.value = arr[index].value;
+	estimationText.value = arr[index].text
 }
+
+(() => {
+	index = 0;
+	arr = [
+		{text: "Ala Bala", value: 1},
+		{text: "KO STAA", value: 3},
+		{text: "pesho be tuk", value: 4.5},
+		{text: "i sya kvo?", value: 5},
+		{text: "ko, ne", value: 1.5},
+		{text: "pylen minus", value: 2}
+	];
+
+	const estimationText = document.getElementById('estimation-info-text');
+	const estimationValue = document.getElementById('est-value-disabled');
+	estimationValue.value = arr[index].value;
+	estimationText.value = arr[index].text
+})();
--- js/open-close.js
+++ js/open-close.js
@@ -2,6 +2,10 @@ function openForm(elementId) {
     document.getElementById(elementId).style.display = "flex";
 }
 
+function closeForm(elementId) {
+    document.getElementById(elementId).style.display = "none";
+}
+
 function closeForm(event, elementId) {
 	if (event.target.id === elementId)
     document.getElementById(elementId).style.display = "none";
